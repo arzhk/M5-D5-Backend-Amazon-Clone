f979311e85f495cc5ccab6f4a3f8b778a73b54d9
arzhk <72197750+arzhk@users.noreply.github.com>
2020-12-11 10:03:08 +0000

    Added functionality
    - Added CRUD functionality and error handling for products.

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { join } = require("path");
+const cors = require("cors");
+const studentsRoutes = require("./products");
+const {
+  notFoundHandler,
+  unauthorizedHandler,
+  forbiddenHandler,
+  badRequestHandler,
+  catchAllHandler,
+} = require("./errorHandling");
+
+const server = express();
+
+const port = 3001;
+const publicFolderPath = join(__dirname, "../public");
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(`Logged ${req.url} ${req.method} -- ${new Date()}`);
+  next();
+};
+
+server.use(cors());
+server.use(express.json());
+server.use(loggerMiddleware);
+server.use(express.static(publicFolderPath));
+
+server.use("/products", studentsRoutes);
+
+server.use(notFoundHandler);
+server.use(unauthorizedHandler);
+server.use(forbiddenHandler);
+server.use(badRequestHandler);
+server.use(catchAllHandler);
+
+server.listen(port, () => {
+  console.log("Server is running on port: ", port);
+});
